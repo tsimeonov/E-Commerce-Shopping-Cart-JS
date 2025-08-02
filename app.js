9e4789e3dc6ce57186c1001b7c547eda78d6fc2c
tsimeonov <tsimeonov12@gmail.com>
2025-08-02 16:05:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -46,5 +46,5 @@ document.addEventListener('DOMContentLoaded', () => {
 	const products = new Products();
 
 	// get all products
-	products.getProducts().then((data) => console.log(data));
+	products.getProducts().then((products) => console.log(products));
 });
